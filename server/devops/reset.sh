661a8f22e8b8e7599ee0332476e15a43678bd9e4
Kachanov <kachanov_anton_1997@inbox.ru>
2019-08-20 15:02:33 +0300

    Added devops scripts

---
+++ server/devops/reset.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+PGPASSWORD=postgres psql -h 127.0.0.1 -p 5432 -U postgres -c "DROP DATABASE airline_db;"
+PGPASSWORD=postgres psql -h 127.0.0.1 -p 5432 -U postgres -c "CREATE DATABASE airline_db;"
+PGPASSWORD=postgres psql -h 127.0.0.1 -p 5432 -U postgres -c "GRANT ALL ON DATABASE airline_db TO airline;"
+
+sh devops/fill.sh
